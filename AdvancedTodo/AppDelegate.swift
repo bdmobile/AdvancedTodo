6750dcb4e884e9880e727fd1a723ac816cfa34d0
lpiem <loic.ronzier@etu.univ-lyon1.fr>
2019-03-18 14:07:38 +0100

    [TASK] remove usless import

--- AdvancedTodo/AppDelegate.swift
+++ AdvancedTodo/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -42,6 +41,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-  
 }
 
